ca2a2ab6a47aa20ca16bd01db074bcfa229a836d
MihailsKostjuks <kostjuk.misha@gmail.com>
2025-04-02 12:56:34 +0300

    14

---
+++ function_call_in_select_p_224.sql
@@ -0,0 +1,37 @@
+-- set search_path to postgres_air;
+
+
+-- CREATE OR REPLACE FUNCTION num_passengers(p_flight_id int) RETURNS integer
+-- AS
+-- $$BEGIN
+-- RETURN (
+--  SELECT count(*) FROM booking_leg bl
+--  JOIN booking b USING (booking_id)
+--  JOIN passenger p
+--  USING (booking_id)
+-- WHERE flight_id=p_flight_id);
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- SELECT flight_id,
+--  num_passengers(flight_id) AS num_pass
+-- FROM flight f
+--  WHERE departure_airport='ORD'
+--  AND scheduled_departure BETWEEN '2020-07-05' AND '2020-07-13'; -- > 15 min (couldnt finish)
+
+
+
+SELECT f.flight_id,
+count(*) AS num_pass
+FROM booking_leg bl
+ JOIN booking b USING (booking_id)
+ JOIN passenger p
+ USING (booking_id)
+ JOIN flight f USING (flight_id)
+WHERE departure_airport='ORD'
+AND scheduled_departure BETWEEN '2020-07-05' AND '2020-07-13'
+GROUP BY 1; -- 2s
+
+
+ 
+ 
