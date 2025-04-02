2a3d09a1986a8cca12162f100d206fe734e865b7
MihailsKostjuks <kostjuk.misha@gmail.com>
2025-04-02 11:14:48 +0300

    11

---
+++ views_filtering_before_grouping_p150.sql
@@ -0,0 +1,27 @@
+-- CREATE VIEW flight_stats AS
+-- SELECT bl.flight_id,
+--  departure_airport,
+--  (avg(price))::numeric (7,2) AS avg_price,
+--  count(DISTINCT passenger_id) AS num_passengers
+-- FROM booking b
+--  JOIN booking_leg bl USING (booking_id)
+--  JOIN flight f USING (flight_id)
+--  JOIN passenger p USING (booking_id)
+-- GROUP BY 1,2;
+
+-- SELECT * FROM flight_stats fs
+-- JOIN (SELECT flight_id FROM flight f
+--  WHERE actual_departure between '2020-08-01' and '2020-08-16') fl
+--  ON fl.flight_id=fs.flight_id; -- 1min 28s 
+
+-- SELECT 
+--  bl.flight_id,
+--  departure_airport,
+--  (avg(price))::numeric (7,2) AS avg_price,
+--  count(DISTINCT passenger_id) AS num_passengers
+-- FROM booking b
+-- JOIN booking_leg bl USING (booking_id)
+-- JOIN flight f USING (flight_id)
+-- JOIN passenger p USING (booking_id)
+-- WHERE actual_departure between '2020-08-01' AND '2020-08-16'
+-- GROUP BY 1,2; -- 7s (without using view)
