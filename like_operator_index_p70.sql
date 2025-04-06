b3a724b2d75d0efe8c5d7504f7d7b16c76875737
MihailsKostjuks <kostjuk.misha@gmail.com>
2025-04-06 11:36:42 +0300

    adding pattern search index

--- like_operator_index_p70.sql
+++ like_operator_index_p70.sql
@@ -6,3 +6,9 @@
 
 -- SELECT * FROM account
 --  WHERE (lower(last_name) >='johns' and lower(last_name) < 'johnt'); -- 5ms (uses index)
+
+-- CREATE INDEX account_last_name_lower_pattern
+--  ON account (lower(last_name) text_pattern_ops); -- a pattern search index
+
+SELECT * FROM account
+ WHERE lower(last_name) like 'johns%'; -- 5ms (uses index) (takes advantage of pattern search index)
