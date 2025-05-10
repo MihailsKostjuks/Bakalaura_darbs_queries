b29a8379bdf9ac41aa617b83adceed298461a1f0
MihailsKostjuks <kostjuk.misha@gmail.com>
2025-05-10 18:49:13 +0300

    5th method refactoring

--- method_5/query_3_air.sql
+++ method_5/query_3_air.sql
@@ -1,15 +1,16 @@
-set search_path to postgres_air;
-
-EXPLAIN ANALYZE SELECT DISTINCT ff.first_name, ff.last_name, booking_with_account.price
+-- Baseline
+SELECT DISTINCT ff.first_name, ff.last_name, booking_with_account.price
 FROM (
-    SELECT b.booking_id, b.account_id, b.price, a.frequent_flyer_id
-    FROM booking b
-    INNER JOIN account a ON b.account_id = a.account_id
-    WHERE b.price > 300
+SELECT b.booking_id, b.account_id, b.price, a.frequent_flyer_id
+FROM booking b
+INNER JOIN account a ON b.account_id = a.account_id
+WHERE b.price > 300
 ) AS booking_with_account
 INNER JOIN frequent_flyer ff ON booking_with_account.frequent_flyer_id = ff.frequent_flyer_id
-WHERE ff.first_name LIKE 'J%'
-ORDER BY ff.last_name; -- 1.7s
+WHERE ff.first_name LIKE 'J%';
+
+-- Solution
+DROP MATERIALIZED VIEW IF EXISTS booking_with_account_mv;
 
 CREATE MATERIALIZED VIEW booking_with_account_mv
 AS 
@@ -21,8 +22,4 @@ WHERE b.price > 300;
 SELECT DISTINCT ff.first_name, ff.last_name, booking_with_account_mv.price
 FROM booking_with_account_mv
 INNER JOIN frequent_flyer ff ON booking_with_account_mv.frequent_flyer_id = ff.frequent_flyer_id
-WHERE ff.first_name LIKE 'J%'
-ORDER BY ff.last_name; -- 10.5s
-
--- drop after
-DROP MATERIALIZED VIEW booking_with_account_mv;
+WHERE ff.first_name LIKE 'J%';
--- method_5/query_3_original_dataset.sql
+++ method_5/query_3_original_dataset.sql
@@ -1,29 +1,28 @@
--- SELECT DISTINCT name
--- FROM ( SELECT primary_title , original_title ,
--- crew . title_id , person_id , category
--- FROM crew
--- INNER JOIN titles ON
--- titles . title_id = crew . title_id
--- WHERE primary_title
--- LIKE 'Spider-Man %'
--- OR original_title LIKE 'Spider-Man %') as a
--- INNER JOIN people ON
--- a . person_id = people . person_id
--- WHERE a . category = 'actor'
--- OR a . category = 'actress'; -- 0.77s
+-- Baseline
+SELECT DISTINCT name
+FROM (
+SELECT primary_title, original_title, crew.title_id, person_id, category
+FROM crew
+INNER JOIN titles ON titles.title_id = crew.title_id
+WHERE primary_title LIKE 'Spider-Man %'
+OR original_title LIKE 'Spider-Man %'
+) AS a
+INNER JOIN people ON a.person_id = people.person_id
+WHERE a.category = 'actor'
+OR a.category = 'actress';
+
+-- Solution (both view and query considered in execution time)
+DROP MATERIALIZED VIEW IF EXISTS q3;
 
 CREATE MATERIALIZED VIEW q3
 AS
-SELECT primary_title , original_title ,
-crew . title_id , person_id , category
+SELECT primary_title, original_title, crew.title_id, person_id, category
 FROM crew
-INNER JOIN titles ON
-titles . title_id = crew . title_id
-WHERE primary_title
-LIKE 'Spider-Man %'
+INNER JOIN titles ON titles.title_id = crew.title_id
+WHERE primary_title LIKE 'Spider-Man %'
 OR original_title LIKE 'Spider-Man %';
+
 SELECT DISTINCT name FROM people
-INNER JOIN q3 ON
-q3 . person_id = people . person_id
-WHERE q3 . category = 'actor'
-OR q3 . category = 'actor'; -- 0.69s
+INNER JOIN q3 ON q3.person_id = people.person_id
+WHERE q3.category = 'actor'
+OR q3.category = 'actress';
