69b0241e4be23dd88be83429cc396ec1fbc7d090
MihailsKostjuks <kostjuk.misha@gmail.com>
2025-05-11 12:40:34 +0300

    27th method refactoring

--- method_27/avoiding_offset_airport.sql
+++ method_27/avoiding_offset_airport.sql
@@ -1,15 +1,15 @@
--- set search_path to postgres_air;
-
-explain analyze SELECT * FROM flight 
+-- Baseline
+SELECT * FROM flight 
 WHERE departure_airport = 'ATL' 
 AND status = 'Delayed'
 ORDER BY flight_id
 LIMIT 50
-OFFSET 1000; -- 12ms
+OFFSET 1000;
 
-explain analyze SELECT * FROM flight 
+-- Solution
+SELECT * FROM flight 
 WHERE departure_airport = 'ATL' 
 AND status = 'Delayed'
 AND flight_id > 160384
 ORDER BY flight_id
-FETCH FIRST 50 ROWS ONLY; -- 4ms
+LIMIT 50;
--- method_27/avoiding_offset_imdb.sql
+++ method_27/avoiding_offset_imdb.sql
@@ -1,12 +1,15 @@
-explain analyze select * from title
-where season_nr = 1
-and production_year between 2002 and 2008
-order by id 
-limit 100 offset 5000; -- 228 ms
+-- Baseline
+SELECT * FROM title
+WHERE season_nr = 1
+AND production_year BETWEEN 2002 AND 2008
+ORDER BY id 
+LIMIT 100
+OFFSET 5000;
 
-explain analyze select * from title
-where season_nr = 1
-and production_year between 2002 and 2008
-and id > 54352
-order by id 
-FETCH FIRST 100 ROWS ONLY; -- 205 ms
+-- Solution
+SELECT * FROM title
+WHERE season_nr = 1
+AND production_year BETWEEN 2002 AND 2008
+AND id > 54352
+ORDER BY id 
+LIMIT 100;
